e9e1e2b7d83ccf0d93bf23963deccc8491d15c4d
fbansept <bansept.franck@gmail.com>
2023-06-12 19:07:07 +0200

    change Dockefile for ROOT app

--- spring_mns_23.sql
+++ spring_mns_23.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : jeu. 04 mai 2023 à 01:40
+-- Généré le : lun. 12 juin 2023 à 19:06
 -- Version du serveur : 10.4.28-MariaDB
 -- Version de PHP : 8.2.4
 
@@ -20,6 +20,26 @@ SET time_zone = "+00:00";
 --
 -- Base de données : `spring_mns_23`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `contrat`
+--
+
+CREATE TABLE `contrat` (
+  `id` int(11) NOT NULL,
+  `date_de_creation` date DEFAULT NULL,
+  `date_de_retour` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `contrat`
+--
+
+INSERT INTO `contrat` (`id`, `date_de_creation`, `date_de_retour`) VALUES
+(1, '2023-05-09', '2023-05-11'),
+(2, '2023-05-10', NULL);
 
 -- --------------------------------------------------------
 
@@ -60,6 +80,48 @@ INSERT INTO `entreprise` (`id`, `nom`) VALUES
 (1, 'Amazon'),
 (2, 'Google'),
 (3, 'Red hat');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `ligne_de_contrat`
+--
+
+CREATE TABLE `ligne_de_contrat` (
+  `contrat_id` int(11) NOT NULL,
+  `materiel_id` int(11) NOT NULL,
+  `date_de_retour_anticipe` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `ligne_de_contrat`
+--
+
+INSERT INTO `ligne_de_contrat` (`contrat_id`, `materiel_id`, `date_de_retour_anticipe`) VALUES
+(1, 1, NULL),
+(1, 3, '2023-05-10'),
+(2, 2, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `materiel`
+--
+
+CREATE TABLE `materiel` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(255) DEFAULT NULL,
+  `numero` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `materiel`
+--
+
+INSERT INTO `materiel` (`id`, `nom`, `numero`) VALUES
+(1, 'Ecran 30\"', 123),
+(2, 'Ecran 30\"', 456),
+(3, 'Clavier', 789);
 
 -- --------------------------------------------------------
 
@@ -118,7 +180,29 @@ CREATE TABLE `role` (
 
 INSERT INTO `role` (`id`, `nom`) VALUES
 (1, 'ROLE_UTILISATEUR'),
-(2, 'ROLE_ADMINISTRATEUR');
+(2, 'ROLE_ADMINISTRATEUR'),
+(3, 'ROLE_SUPER_ADMINISTRATEUR');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `role_utilisateur`
+--
+
+CREATE TABLE `role_utilisateur` (
+  `utilisateur_id` int(11) NOT NULL,
+  `role_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `role_utilisateur`
+--
+
+INSERT INTO `role_utilisateur` (`utilisateur_id`, `role_id`) VALUES
+(1, 1),
+(2, 1),
+(2, 2),
+(3, 3);
 
 -- --------------------------------------------------------
 
@@ -136,21 +220,27 @@ CREATE TABLE `utilisateur` (
   `prenom` varchar(255) DEFAULT NULL,
   `updated_at` datetime DEFAULT NULL,
   `entreprise_id` int(11) DEFAULT NULL,
-  `pays_id` int(11) DEFAULT NULL,
-  `role_id` int(11) DEFAULT NULL
+  `pays_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Déchargement des données de la table `utilisateur`
 --
 
-INSERT INTO `utilisateur` (`id`, `created_at`, `email`, `mot_de_passe`, `nom`, `nom_image_profil`, `prenom`, `updated_at`, `entreprise_id`, `pays_id`, `role_id`) VALUES
-(1, '2023-01-01', 'jd@a.com', '$2a$10$wXW2wHA2bu1TdQ26p.2UoehWv8m92w88kabSeL.348VqkpWvSt51q', 'doe', NULL, 'John', '2023-01-03 00:00:00', 1, 2, 1),
-(2, '2023-05-03', 'fb@a.com', '$2a$10$wXW2wHA2bu1TdQ26p.2UoehWv8m92w88kabSeL.348VqkpWvSt51q', 'bansept', NULL, 'Franck', '2023-05-03 14:56:26', 1, 2, 2);
+INSERT INTO `utilisateur` (`id`, `created_at`, `email`, `mot_de_passe`, `nom`, `nom_image_profil`, `prenom`, `updated_at`, `entreprise_id`, `pays_id`) VALUES
+(1, '2023-01-01', 'jd@a.com', '$2a$10$wXW2wHA2bu1TdQ26p.2UoehWv8m92w88kabSeL.348VqkpWvSt51q', 'doe', NULL, 'John', '2023-01-03 00:00:00', 1, 2),
+(2, '2023-06-12', 'fb@a.com', '$2a$10$wXW2wHA2bu1TdQ26p.2UoehWv8m92w88kabSeL.348VqkpWvSt51q', 'bansept', NULL, 'Franck', '2023-06-12 17:05:13', 1, 2),
+(3, '2023-06-12', 'as@a.com', '$2a$10$wXW2wHA2bu1TdQ26p.2UoehWv8m92w88kabSeL.348VqkpWvSt51q', 'Super', NULL, 'Admin', '2023-06-12 17:05:13', 1, 2);
 
 --
 -- Index pour les tables déchargées
 --
+
+--
+-- Index pour la table `contrat`
+--
+ALTER TABLE `contrat`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Index pour la table `emploi`
@@ -162,6 +252,19 @@ ALTER TABLE `emploi`
 -- Index pour la table `entreprise`
 --
 ALTER TABLE `entreprise`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `ligne_de_contrat`
+--
+ALTER TABLE `ligne_de_contrat`
+  ADD PRIMARY KEY (`contrat_id`,`materiel_id`),
+  ADD KEY `FK7wwltaxcl89fe8tyt2ieyupwv` (`materiel_id`);
+
+--
+-- Index pour la table `materiel`
+--
+ALTER TABLE `materiel`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -184,17 +287,29 @@ ALTER TABLE `role`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Index pour la table `role_utilisateur`
+--
+ALTER TABLE `role_utilisateur`
+  ADD KEY `FKtqsv7mkho8vm7i3x5n65a0fr2` (`role_id`),
+  ADD KEY `FKey0mbtbkylluasjnxhfrt042u` (`utilisateur_id`);
+
+--
 -- Index pour la table `utilisateur`
 --
 ALTER TABLE `utilisateur`
   ADD PRIMARY KEY (`id`),
   ADD KEY `FK8fjtucbyo2t6agaejym2j764f` (`entreprise_id`),
-  ADD KEY `FKc1g8do1rrrp4bwytrl73elnou` (`pays_id`),
-  ADD KEY `FKaqe8xtajee4k0wlqrvh2pso4l` (`role_id`);
+  ADD KEY `FKc1g8do1rrrp4bwytrl73elnou` (`pays_id`);
 
 --
 -- AUTO_INCREMENT pour les tables déchargées
 --
+
+--
+-- AUTO_INCREMENT pour la table `contrat`
+--
+ALTER TABLE `contrat`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT pour la table `emploi`
@@ -209,6 +324,12 @@ ALTER TABLE `entreprise`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
+-- AUTO_INCREMENT pour la table `materiel`
+--
+ALTER TABLE `materiel`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
 -- AUTO_INCREMENT pour la table `pays`
 --
 ALTER TABLE `pays`
@@ -218,17 +339,24 @@ ALTER TABLE `pays`
 -- AUTO_INCREMENT pour la table `role`
 --
 ALTER TABLE `role`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT pour la table `utilisateur`
 --
 ALTER TABLE `utilisateur`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Contraintes pour les tables déchargées
 --
+
+--
+-- Contraintes pour la table `ligne_de_contrat`
+--
+ALTER TABLE `ligne_de_contrat`
+  ADD CONSTRAINT `FK5tui6uo11yoq0ag9vne4m1o55` FOREIGN KEY (`contrat_id`) REFERENCES `contrat` (`id`),
+  ADD CONSTRAINT `FK7wwltaxcl89fe8tyt2ieyupwv` FOREIGN KEY (`materiel_id`) REFERENCES `materiel` (`id`);
 
 --
 -- Contraintes pour la table `recherche_emploi_utilisateur`
@@ -238,11 +366,17 @@ ALTER TABLE `recherche_emploi_utilisateur`
   ADD CONSTRAINT `FKm97nfoc6ask1llg09q3mppqre` FOREIGN KEY (`emploi_id`) REFERENCES `emploi` (`id`);
 
 --
+-- Contraintes pour la table `role_utilisateur`
+--
+ALTER TABLE `role_utilisateur`
+  ADD CONSTRAINT `FKey0mbtbkylluasjnxhfrt042u` FOREIGN KEY (`utilisateur_id`) REFERENCES `utilisateur` (`id`),
+  ADD CONSTRAINT `FKtqsv7mkho8vm7i3x5n65a0fr2` FOREIGN KEY (`role_id`) REFERENCES `role` (`id`);
+
+--
 -- Contraintes pour la table `utilisateur`
 --
 ALTER TABLE `utilisateur`
   ADD CONSTRAINT `FK8fjtucbyo2t6agaejym2j764f` FOREIGN KEY (`entreprise_id`) REFERENCES `entreprise` (`id`),
-  ADD CONSTRAINT `FKaqe8xtajee4k0wlqrvh2pso4l` FOREIGN KEY (`role_id`) REFERENCES `role` (`id`),
   ADD CONSTRAINT `FKc1g8do1rrrp4bwytrl73elnou` FOREIGN KEY (`pays_id`) REFERENCES `pays` (`id`);
 COMMIT;
 
